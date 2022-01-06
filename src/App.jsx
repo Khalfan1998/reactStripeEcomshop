f2a5a616db20018310496912daf9c79f1e834321
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-06 10:52:13 +0300

    Router
    adding router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Success from "./Success";
 import Pay from "./Pay";
 
 const App = () => {
     return (
         <Router>
-            <Switch>
-                <Route path ="/pay">
-                    <Pay />
-                </Route>
-                <Route path = "/success">
-                    <Success />
-                </Route>
-            </Switch>
-        </Router>
+          <Routes>
+            <Route 
+            path="/Pay" 
+            element={<Pay />} 
+            />
+            <Route 
+            path="/Success" 
+            element={<Success />} 
+            />
+          </Routes>
+      </Router>
     );
 };
 
--- src/Pay.jsx
+++ src/Pay.jsx
@@ -1,6 +1,14 @@
 import StripeCheckout from 'react-stripe-checkout';
 
+const KEY = "pk_test_51K9luKDHgD7vvY49C8bHgEUCNYjaGuaA0NF9clG1WMRNctR3dINn6HFElfUsoPlgRFGpjnYAiZTAhrKkeIscPKOR00wBTuQuns"
+
 const Pay = () => {
+
+    const onToken = (token) => {
+        console.log(token)
+
+    }
+
     return (
         <div
         style={{
@@ -10,6 +18,18 @@ const Pay = () => {
             justifyContent: "center",
         }}
         >
+            <StripeCheckout
+            name="Khalfan Shop"
+            image="https://i.postimg.cc/pTFsScVB/display2-removebg-preview.png"
+            billingAddress
+            shippingAddress
+            description="Your total is $30"
+            amount={3000}
+            token={onToken}
+            stripeKey= {KEY}
+            
+            
+            >
             <button
             style={{
                 border: "none",
@@ -25,6 +45,7 @@ const Pay = () => {
             >
                 Pay Now
             </button>
+            </StripeCheckout>
         </div>
     )
 }
