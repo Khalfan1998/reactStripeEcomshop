9cc3228bc165453b115ff706e44e8025161e42d9
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-06 11:51:21 +0300

    Success page
    adding success page

--- src/Success.jsx
+++ src/Success.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 
 const Success = () => {
     return (
-        <div>
-            
-        </div>
+        <div
+        style={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+        }}
+        >
+            <img src ="https://i.postimg.cc/pTFsScVB/display2-removebg-preview.png" alt="logo"
+            style={{
+            width: "100px",
+          
+            }}
+            />
+
+            <button
+            style={{
+                border: "none",
+                width: 120,
+                borderRadius: 5,
+                padding: "20px",
+                backgroundColor: "teal",
+                color: "white",
+                fontWeight: "600",
+                
+            }}
+              
+            >
+                Successfull
+            </button>
+            <p>Your order is being processed. Thanks for choosing Khalfan Shop</p>
+
+            </div>
     )
 }
 
